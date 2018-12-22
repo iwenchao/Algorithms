a592960ede7ac8e30c5b9f479f220aca2d4fa5ba
iwenchao <iwenchaos@gmail.com>
2018-12-22 15:21:47 +0800

    数组移位，快慢指针问题

--- mdu.algor/src/main/java/com/iwenchaos/mdualgor/array/MoveZeroAlgo.java
+++ mdu.algor/src/main/java/com/iwenchaos/mdualgor/array/MoveZeroAlgo.java
@@ -28,6 +28,10 @@ public class MoveZeroAlgo {
     /**
      * 这类数组原地删除数据的题目，考察的就是两根指针的应用，
      *
+     * 我们设置两根指针slow和fast，fast每次都递增，所以称之为fast，当遍历到不是0的时候，slow指针就存储fast此时遍历的元素，slow加一，当遍历到需要删除的元素0(就相当于移动0到最后)时，略过slow，直接fast++，相当于删除了0.同时设置一个变量记录新数组的长度。
+     * 最后我们再遍历剩下的元素，并给他们赋值为0即可。
+     * ** 这道题可以看作是删除元素那道题的扩展应用**
+     *
      * @param nums
      */
     public static int[] moveZeroes(int[] nums) {
