ddb0994b318815161d64ead0a0c20b284466d61f
iwenchao <iwenchaos@gmail.com>
2018-12-26 11:23:51 +0800

    链表

---
+++ mdu.algor/src/main/java/com/iwenchaos/mdualgor/link/DelNodeAlgo.java
@@ -0,0 +1,43 @@
+package com.iwenchaos.mdualgor.link;
+
+/**
+ * Created by chaos
+ * on 2018/12/26. 11:09
+ * 文件描述：
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ * <p>
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ * <p>
+ * 4 -> 5 -> 1 -> 9
+ * 示例 1:
+ * <p>
+ * 输入: head = [4,5,1,9], node = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ * <p>
+ * 输入: head = [4,5,1,9], node = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ * 说明:
+ * <p>
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ */
+public class DelNodeAlgo {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private static void deleteNode(ListNode node) {
+        node.val = node.next.val;//将待删除node节点的下一个节点value赋值给del node
+        node.next = node.next.next;////将待删除node节点的下一个节点的next赋值给del node next
+    }
+
+
+}
---
+++ mdu.algor/src/main/java/com/iwenchaos/mdualgor/link/ListNode.java
@@ -0,0 +1,15 @@
+package com.iwenchaos.mdualgor.link;
+
+/**
+ * Created by chaos
+ * on 2018/12/26. 11:10
+ * 文件描述：
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
