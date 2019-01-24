7b1129226a7e7d268435dd2579058a12ad3f7aa4
iwenchao <iwenchaos@gmail.com>
2019-01-24 09:46:06 +0800

    深度遍历二叉树（stack结构遍历）以及广度遍历二叉树(queue结构遍历）

--- mdu.algor/src/main/java/com/iwenchaos/mdualgor/tree/Traverse2XTreeAlgo.java
+++ mdu.algor/src/main/java/com/iwenchaos/mdualgor/tree/Traverse2XTreeAlgo.java
@@ -1,5 +1,7 @@
 package com.iwenchaos.mdualgor.tree;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -20,17 +22,18 @@ public class Traverse2XTreeAlgo {
         inorderTraverse(root);
 //        preorderTraverse(root);
     }
-    public static void createBinTree(TreeNode root){
+
+    public static void createBinTree(TreeNode root) {
         TreeNode newNodeB = new TreeNode(2);
         TreeNode newNodeC = new TreeNode(3);
         TreeNode newNodeD = new TreeNode(4);
         TreeNode newNodeE = new TreeNode(5);
         TreeNode newNodeF = new TreeNode(6);
-        root.left=newNodeB;
-        root.right=newNodeC;
-        root.left.left=newNodeD;
-        root.left.right=newNodeE;
-        root.right.right=newNodeF;
+        root.left = newNodeB;
+        root.right = newNodeC;
+        root.left.left = newNodeD;
+        root.left.right = newNodeE;
+        root.right.right = newNodeF;
     }
 
     /**
@@ -40,48 +43,124 @@ public class Traverse2XTreeAlgo {
         if (root == null) {
             return;
         }
-        System.out.print(root.value+"-->");
+        System.out.print(root.value + "-->");
         preorderTraverse(root.left);
         preorderTraverse(root.right);
 
     }
+
     /**
      * 递归方式
      */
-    public static void inorderTraverse(TreeNode root){
-        if(root == null){
+    public static void inorderTraverse(TreeNode root) {
+        if (root == null) {
             return;
         }
         inorderTraverse(root.left);
-        System.out.print(root.value+"-->");
+        System.out.print(root.value + "-->");
         inorderTraverse(root.right);
     }
+
     /**
      * 递归方式
      */
-    public static void posorderTraverse(TreeNode root){
-        if(root == null) return;
+    public static void posorderTraverse(TreeNode root) {
+        if (root == null) return;
         preorderTraverse(root.left);
         preorderTraverse(root.right);
-        System.out.print(root.value+"-->");
+        System.out.print(root.value + "-->");
     }
 
 
-    public static void preorderTraverse2(TreeNode root){
+    /**
+     * 采用非递归的方式，使得已遍历的节点不再遍历
+     * <p>
+     * 前序遍历二叉树
+     */
+    public static void preorderTraverse2(TreeNode root) {
         if (root == null) return;
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             root = stack.pop();
-            System.out.print(root.value+"-->");
-            if(root.right!= null){
+            System.out.print(root.value + "-->");
+            if (root.right != null) {
                 stack.push(root.right);
             }
-            if(root.left!= null){
+            if (root.left != null) {
                 stack.push(root.left);
             }
         }
 
+    }
+
+    /**
+     * 采用非递归的方式，使得已遍历的节点不再遍历
+     * <p>
+     * 中序遍历二叉树 左-根-右
+     */
+    public static void inorderTraverse2(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {//root为空说明走到了叶子节点
+                root = stack.pop();
+                System.out.print(root.value + "-->");
+                root = root.right;
+            }
+        }
+
+
+    }
+
+    /**
+     * 采用非递归的方式，使得已遍历的节点不再遍历
+     * <p>
+     * 后序遍历二叉树 左-右-根
+     * 本方法的思想就是 逆向，将逆向遍历的结果放到stack中，
+     * 然后在从stack pop（）出来就是正向的后序遍历
+     */
+    public static void posorderTraverse2(TreeNode root) {
+        if (root == null) return;
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        s1.push(root);
+        while (!s1.isEmpty()) {
+            root = s1.pop();
+            s2.push(root);
+            if (root.left != null) {
+                s1.push(root.left);
+            }
+            if (root.right != null) {
+                s1.push(root.right);
+            }
+        }
+        while (!s2.isEmpty()) {
+            System.out.print(s2.pop().value + "-->");
+        }
+    }
+
+
+    /**
+     * 广度遍历 二叉树
+     */
+    public static void flatOrderTraverse(TreeNode root) {
+        if (root == null) return;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            root = ((ArrayDeque<TreeNode>) queue).pop();
+            System.out.print(root.value + "-->");
+            if (root.left != null) {
+                queue.add(root.left);
+            }
+            if (root.right != null) {
+                queue.add(root.right);
+            }
+        }
     }
 
 }
